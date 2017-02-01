3880c9ab72d83e85ff895caa042a7521c1607c64
Rohan Roy <b4you0870@gmail.com>
2017-02-01 22:44:57 +0530

    static files configuration was updated

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-release: python manage.py collectstatic --noinput
+web: python manage.py collectstatic --noinput
 web: gunicorn github_committers.wsgi --preload
--- github_committers/urls.py
+++ github_committers/urls.py
@@ -14,7 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
 from django.conf.urls import include, url
+from django.conf import settings
 
 urlpatterns = [
     url(r'^apis/', include("apis.urls", namespace="apis")),
+]
+
+urlpatterns += [
+    url(r'^static/(?P.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 ]
