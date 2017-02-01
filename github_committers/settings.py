a9e6447c890a6ad1bd320c5a5db8e869c664fa57
Rohan Roy <b4you0870@gmail.com>
2017-02-01 22:40:16 +0530

    settings was updated

--- github_committers/settings.py
+++ github_committers/settings.py
@@ -37,9 +37,9 @@ if exists(env_file):
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
