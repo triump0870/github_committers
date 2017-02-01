0f0896021c45fc14e39d53c75dae9cfc2a0068f5
Rohan Roy <b4you0870@gmail.com>
2017-02-01 21:56:58 +0530

    typos were corrected

--- github_committers/urls.py
+++ github_committers/urls.py
@@ -1,4 +1,4 @@
-"""github_toppers URL Configuration
+"""github_committers URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.8/topics/http/urls/
@@ -14,7 +14,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
 from django.conf.urls import include, url
-from django.contrib import admin
 
 urlpatterns = [
     url(r'^apis/', include("apis.urls", namespace="apis")),
--- github_committers/wsgi.py
+++ github_committers/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for github_toppers project.
+WSGI config for github_committers project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
