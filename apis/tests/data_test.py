c78ed63bd9c325453c2ccc73c973b1f1dbb41a89
Rohan Roy <b4you0870@gmail.com>
2017-02-02 18:16:27 +0530

    tests were added

---
+++ apis/tests/data_test.py
@@ -0,0 +1,8 @@
+error_data = {
+        "detail": "{'documentation_url': 'https://developer.github.com/v3', 'message': 'Not Found'}",
+        "status_code": 404
+    }
+
+exist_data = {
+    "status_code": 200
+}
--- apis/tests/tests.py
+++ apis/tests/tests.py
@@ -3,20 +3,18 @@ from apis.serializers import OrganizationNameSerializer
 from faker import Faker
 from unittest import mock
 from django.http.response import HttpResponse
+from apis.utils import AttrDict
+from apis.tests import data_test
+from rest_framework.exceptions import NotFound
 
 
 # Create your tests here.
 class OrganizationSerializerTest(TestCase):
-    error_data = {
-        "detail": "{'documentation_url': 'https://developer.github.com/v3', 'message': 'Not Found'}",
-        "status_code": 404
-    }
-
     def setUp(self):
         fake = Faker()
 
         self.data = {
-            "name": fake.name
+            "name": fake.name()
         }
 
     def test_required_fields(self):
@@ -45,13 +43,25 @@ class OrganizationSerializerTest(TestCase):
         self.assertEqual(serializer.errors, expected_error)
 
     @mock.patch("apis.serializers.requests.get",
-                return_value=error_data)
+                return_value=AttrDict(data_test.error_data))
     def test_org_not_found(self, *args):
         data = {
             "name": "abcdefgs"
         }
         serializer = OrganizationNameSerializer(data=data)
+
+        with self.assertRaisesMessage(NotFound, ""):
+            is_valid = serializer.is_valid()
+            self.assertFalse(is_valid)
+
+    @mock.patch("apis.serializers.requests.get",
+                return_value=AttrDict(data_test.exist_data))
+    def test_org_exist(self, *args):
+        serializer = OrganizationNameSerializer(data=self.data)
+
         is_valid = serializer.is_valid()
 
-        print("valid: ", is_valid)
-        print("error: ", serializer.errors)
+        expected_data = self.data
+
+        self.assertTrue(is_valid)
+        self.assertEqual(serializer.data, expected_data)
